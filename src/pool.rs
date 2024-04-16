6f18787008763503cb3089eb650db449923662cd
Rouzbeh Sabzehei <rouzbehsbz@gmail.com>
2024-04-16 17:05:17 +0330

    feat: add pool module with worker struct

--- src/pool.rs
+++ src/pool.rs
@@ -1,1 +1,34 @@
-pub struct ThreadPool {}
+use std::{
+    sync::{mpsc::Receiver, Arc, Mutex},
+    thread::{self, JoinHandle},
+};
+
+type SafeReceiver = Arc<Mutex<Receiver<Job>>>;
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+pub struct Worker {
+    thread: JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(receiver: SafeReceiver) -> Self {
+        let thread = thread::spawn(move || {
+            //TODO: Make sure lock is not poisoned here
+            loop {
+                let job = receiver.lock().unwrap().try_recv();
+
+                if let Ok(job) = job {
+                    job();
+                } else {
+                    break;
+                }
+            }
+        });
+
+        Self { thread }
+    }
+}
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+}

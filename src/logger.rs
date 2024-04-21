432aa898212620d61f39afab0112a9e6f0dce55e
Rouzbeh Sabzehei <rouzbehsbz@gmail.com>
2024-04-21 12:15:05 +0330

    fix: use more helpful logs

--- src/logger.rs
+++ src/logger.rs
@@ -8,16 +8,23 @@ pub struct Logger;
 
 impl Logger {
     pub fn show_overview(arguments: &Arguments, storage: Arc<Storage>) -> AppResult<()> {
-        println!("Target: ({}) {}", arguments.method, arguments.target);
+        println!(
+            "Benchamrking Target: ({}) {}",
+            arguments.method, arguments.target
+        );
 
         if let Some(proxy) = &arguments.proxy {
-            println!("Proxy: {}", proxy)
+            println!("Using proxy: {}", proxy)
         }
 
-        println!("Connections: {}", arguments.connections);
-        println!("Total Requests: {}", storage.get_total_requests_count()?);
         println!(
-            "Average Response Time: {} ms \n",
+            "Total {} requests have been sent over {} seconds and {} connections.",
+            storage.get_total_requests_count()?,
+            arguments.duration,
+            arguments.connections
+        );
+        println!(
+            "Average response time: {} ms \n",
             storage.get_total_avg_response_time()?
         );
 
@@ -32,52 +39,64 @@ impl Logger {
         table.add_row(row![
             "Type",
             "Total",
-            "Avg Time (ms)",
-            "Lowest Time (ms)",
-            "Highest Time (ms)"
+            "Avg (ms)",
+            "Lowest (ms)",
+            "Highest (ms)"
         ]);
-        table.add_row(row![
-            "Success (2xx)",
-            storage.get_requests_count(StatusCodeCategory::Success)?,
-            storage.get_avg_response_time(StatusCodeCategory::Success)?,
-            storage.get_min_reponse_time(StatusCodeCategory::Success)?,
-            storage.get_max_reponse_time(StatusCodeCategory::Success)?,
-        ]);
-        table.add_row(row![
-            "Redirection (3xx)",
-            storage.get_requests_count(StatusCodeCategory::Redirection)?,
-            storage.get_avg_response_time(StatusCodeCategory::Redirection)?,
-            storage.get_min_reponse_time(StatusCodeCategory::Redirection)?,
-            storage.get_max_reponse_time(StatusCodeCategory::Redirection)?,
-        ]);
-        table.add_row(row![
-            "Client Error (4xx)",
-            storage.get_requests_count(StatusCodeCategory::ClientError)?,
-            storage.get_avg_response_time(StatusCodeCategory::ClientError)?,
-            storage.get_min_reponse_time(StatusCodeCategory::ClientError)?,
-            storage.get_max_reponse_time(StatusCodeCategory::ClientError)?,
-        ]);
-        table.add_row(row![
-            "Server Error (5xx)",
-            storage.get_requests_count(StatusCodeCategory::ServerError)?,
-            storage.get_avg_response_time(StatusCodeCategory::ServerError)?,
-            storage.get_min_reponse_time(StatusCodeCategory::ServerError)?,
-            storage.get_max_reponse_time(StatusCodeCategory::ServerError)?,
-        ]);
-        table.add_row(row![
-            "Informational (1xx)",
-            storage.get_requests_count(StatusCodeCategory::Informational)?,
-            storage.get_avg_response_time(StatusCodeCategory::Informational)?,
-            storage.get_min_reponse_time(StatusCodeCategory::Informational)?,
-            storage.get_max_reponse_time(StatusCodeCategory::Informational)?,
-        ]);
-        table.add_row(row![
-            "Failed",
-            storage.get_requests_count(StatusCodeCategory::Failed)?,
-            storage.get_avg_response_time(StatusCodeCategory::Failed)?,
-            storage.get_min_reponse_time(StatusCodeCategory::Failed)?,
-            storage.get_max_reponse_time(StatusCodeCategory::Failed)?,
-        ]);
+        if storage.get_requests_count(StatusCodeCategory::Success)? != 0 {
+            table.add_row(row![
+                "2xx",
+                storage.get_requests_count(StatusCodeCategory::Success)?,
+                storage.get_avg_response_time(StatusCodeCategory::Success)?,
+                storage.get_min_reponse_time(StatusCodeCategory::Success)?,
+                storage.get_max_reponse_time(StatusCodeCategory::Success)?,
+            ]);
+        }
+        if storage.get_requests_count(StatusCodeCategory::Redirection)? != 0 {
+            table.add_row(row![
+                "3xx",
+                storage.get_requests_count(StatusCodeCategory::Redirection)?,
+                storage.get_avg_response_time(StatusCodeCategory::Redirection)?,
+                storage.get_min_reponse_time(StatusCodeCategory::Redirection)?,
+                storage.get_max_reponse_time(StatusCodeCategory::Redirection)?,
+            ]);
+        }
+        if storage.get_requests_count(StatusCodeCategory::ClientError)? != 0 {
+            table.add_row(row![
+                "4xx",
+                storage.get_requests_count(StatusCodeCategory::ClientError)?,
+                storage.get_avg_response_time(StatusCodeCategory::ClientError)?,
+                storage.get_min_reponse_time(StatusCodeCategory::ClientError)?,
+                storage.get_max_reponse_time(StatusCodeCategory::ClientError)?,
+            ]);
+        }
+        if storage.get_requests_count(StatusCodeCategory::ServerError)? != 0 {
+            table.add_row(row![
+                "5xx",
+                storage.get_requests_count(StatusCodeCategory::ServerError)?,
+                storage.get_avg_response_time(StatusCodeCategory::ServerError)?,
+                storage.get_min_reponse_time(StatusCodeCategory::ServerError)?,
+                storage.get_max_reponse_time(StatusCodeCategory::ServerError)?,
+            ]);
+        }
+        if storage.get_requests_count(StatusCodeCategory::Informational)? != 0 {
+            table.add_row(row![
+                "1xx",
+                storage.get_requests_count(StatusCodeCategory::Informational)?,
+                storage.get_avg_response_time(StatusCodeCategory::Informational)?,
+                storage.get_min_reponse_time(StatusCodeCategory::Informational)?,
+                storage.get_max_reponse_time(StatusCodeCategory::Informational)?,
+            ]);
+        }
+        if storage.get_requests_count(StatusCodeCategory::Failed)? != 0 {
+            table.add_row(row![
+                "Failed",
+                storage.get_requests_count(StatusCodeCategory::Failed)?,
+                storage.get_avg_response_time(StatusCodeCategory::Failed)?,
+                storage.get_min_reponse_time(StatusCodeCategory::Failed)?,
+                storage.get_max_reponse_time(StatusCodeCategory::Failed)?,
+            ]);
+        }
 
         //TODO: alien method! can causd panice
         table.printstd();
---
+++ src/parser.rs
@@ -0,0 +1,181 @@
+use clap::{arg, command, error::Result, value_parser, ArgMatches, Error};
+use reqwest::{
+    header::{HeaderMap, HeaderName, HeaderValue},
+    Method,
+};
+use std::path::PathBuf;
+
+#[derive(Debug)]
+pub struct Arguments {
+    pub target: String,
+    pub method: Method,
+    pub proxy: Option<String>,
+    pub duration: u64,
+    pub connections: usize,
+    pub payload: Option<String>,
+    pub output: Option<PathBuf>,
+    pub headers: Option<HeaderMap>,
+    pub timeout: u64,
+}
+
+pub struct Parser {
+    matches: ArgMatches,
+}
+
+impl Parser {
+    pub fn new() -> Self {
+        let matches = command!()
+            .about("Cicadas is a fast multi threaded HTTP load testing tool.")
+            .version("1.0.0")
+            .arg(
+                arg!(
+                    -t --target <target> "Target URL for applying load test"
+                )
+                .required(true)
+                .value_parser(value_parser!(String)),
+            )
+            .arg(
+                arg!(
+                    -m --method <method> "HTTP method for load testing the target"
+                )
+                .required(true)
+                .value_parser(value_parser!(String)),
+            )
+            .arg(
+                arg!(
+                    -P --proxy <proxy> "Optional Proxy URL to use when testing target"
+                )
+                .required(false)
+                .value_parser(value_parser!(String)),
+            )
+            .arg(
+                arg!(
+                    -d --duration <duration> "Load test duration in seconds"
+                )
+                .required(true)
+                .value_parser(value_parser!(u64)),
+            )
+            .arg(
+                arg!(
+                    -c --connections <connections> "Connections count to open and map with OS threads"
+                )
+                .required(true)
+                .value_parser(value_parser!(usize)),
+            )
+            .arg(
+                arg!(
+                    -p --payload <payload> "Optional body payload to pass to the target"
+                )
+                .required(false)
+                .value_parser(value_parser!(String)),
+            )
+            .arg(
+                arg!(
+                    -o --output <output> "Optional file path to store target responses"
+                )
+                .required(false)
+                .value_parser(value_parser!(PathBuf)),
+            )
+            .arg(
+                arg!(
+                    -H --headers <headers> "Optional HTTP headers for load testing the target"
+                )
+                .num_args(0..=255)
+                .required(false)
+                .value_parser(value_parser!(String)),
+            )
+            .arg(
+                arg!(
+                    -T --timeout <timeout> "Optional HTTP request timeout duration in seconds"
+                )
+                .required(false)
+                .default_value("5")
+                .value_parser(value_parser!(u64)),
+            )
+            .get_matches();
+
+        Self { matches }
+    }
+
+    pub fn parse_headers(headers: Vec<String>) -> Result<Option<HeaderMap>> {
+        let mut headers_map = HeaderMap::with_capacity(headers.len());
+
+        for header in headers {
+            let splitted_header: Vec<&str> = header.split(':').collect();
+
+            //TODO: maybe can do better error handling with clap
+            if splitted_header.len() != 2 {
+                return Err(Error::new(clap::error::ErrorKind::InvalidValue));
+            }
+
+            //TODO: handle errors here
+            let key: HeaderName = splitted_header[0].parse().unwrap();
+            let value: HeaderValue = splitted_header[1].parse().unwrap();
+
+            headers_map.insert(key, value);
+        }
+
+        Ok(Some(headers_map))
+    }
+
+    pub fn parse_method(method: String) -> Result<Method> {
+        let method = method.to_uppercase();
+
+        match method.as_str() {
+            "GET" => Ok(Method::GET),
+            "POST" => Ok(Method::POST),
+            "PUT" => Ok(Method::PUT),
+            "DELETE" => Ok(Method::DELETE),
+            "HEAD" => Ok(Method::HEAD),
+            "OPTIONS" => Ok(Method::OPTIONS),
+            //TODO: maybe can do better error handling with clap
+            _ => Err(Error::new(clap::error::ErrorKind::InvalidValue)),
+        }
+    }
+
+    pub fn get_arguments(&self) -> Result<Arguments> {
+        let target = self.matches.get_one::<String>("target").unwrap().to_owned();
+        let raw_method = self.matches.get_one::<String>("method").unwrap().to_owned();
+        let method = Self::parse_method(raw_method)?;
+        let proxy = match self.matches.get_one::<String>("proxy").to_owned() {
+            Some(proxy) => Some(proxy.to_owned()),
+            None => None,
+        };
+        let duration = self.matches.get_one::<u64>("duration").unwrap().to_owned();
+        let timeout = self.matches.get_one::<u64>("timeout").unwrap().to_owned();
+        let connections = self
+            .matches
+            .get_one::<usize>("connections")
+            .unwrap()
+            .to_owned();
+        let payload = match self.matches.get_one::<String>("payload").to_owned() {
+            Some(payload) => Some(payload.to_owned()),
+            None => None,
+        };
+        let output = match self.matches.get_one::<PathBuf>("output").to_owned() {
+            Some(output) => Some(output.to_owned()),
+            None => None,
+        };
+        let raw_headers: Vec<String> = match self.matches.get_many("headers") {
+            Some(headers) => {
+                let headers: Vec<String> = headers.cloned().collect();
+
+                headers
+            }
+            None => Vec::with_capacity(0),
+        };
+        let headers = Self::parse_headers(raw_headers)?;
+
+        Ok(Arguments {
+            target,
+            method,
+            proxy,
+            duration,
+            connections,
+            payload,
+            output,
+            headers,
+            timeout,
+        })
+    }
+}
